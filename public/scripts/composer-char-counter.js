179054b492cbb45dc850b52ea5780fa5723ccf3b
mohaelmi <moha.abdi1771@gmail.com>
2023-07-10 07:02:31 -0500

    clean up

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,18 @@
 $(document).ready(function() {
-  $('#tweet-text').on('input', function(e) {
-    const counterLimit = 140
+  $('#tweet-text').on('input', function() {
+    const counterLimit = 140;
     //grap tweet-text id and counter class using 'this'
     let val = $(`#${this.id}`).val();
-    let counter = $('.' + $(`#${this.id}`).parents()[0].counter.name)
+    let counter = $('.' + $(`#${this.id}`).parents()[0].counter.name);
     
-    let counting = counterLimit - val.length
-    if(counting < 0 ){
-      $(counter).css('background-color', 'red')
-      $(counter).val(counting)
-    }else{
-      $(counter).css('background-color', 'transparent')
-      $(counter).val(counting)
+    let counting = counterLimit - val.length;
+    if (counting < 0) {
+      $(counter).css('background-color', 'red');
+      $(counter).val(counting);
+    } else {
+      $(counter).css('background-color', 'transparent');
+      $(counter).val(counting);
     }
-  })
-  // --- our code goes here ---
+  });
 });
 
