1c9c7ace2ee521ae71aca9396542412f9d02c477
mohaelmi <moha.abdi1771@gmail.com>
2023-07-11 19:39:49 -0500

    render dynamic element/object using template strings

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,4 +4,52 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+$(document).ready(() => {
+
+  const renderTweets = function(tweets) {
+    tweets.forEach(tweet => {
+      let $tweet = createTweetElement(tweet.content.text);
+      $('#tweets-container').append($tweet);
+   
+    });
+  };
+
+  const createTweetElement = function(tweet) {
+    const $tweetMarkup = $(`<article class="tweet">${tweet}</article>`);
+    return $tweetMarkup;
+  };
+
+  renderTweets(data);
+
+});
+
+
+
 
