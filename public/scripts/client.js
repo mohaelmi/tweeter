2d10ac13d15805a16ba0e84ee8d9db6ab79856be
mohaelmi <moha.abdi1771@gmail.com>
2023-07-13 17:20:08 -0500

    render data without page refreshing

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,67 +1,36 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ];
-
-
 $(document).ready(() => {
 
   //fetcting data/tweets from the server
   const loadTweets = function() {
-    $.ajax({ 
-      url: '/tweets', 
+    $.ajax({
+      url: '/tweets',
       method: "GET",
-      success: function (data) {
-       renderTweets(data)
+      success: function(data) {
+        renderTweets(data);
       },
-      fail: function (error) {
+      fail: function(error) {
         console.log("error: ", error);
       }
-    })
-  }
+    });
+  };
 
-  loadTweets()
 
   const renderTweets = function(tweets) {
-    tweets.forEach(tweet => {
+    // tweets.sort((a, b) => timeago.format(b.created_at) - timeago.format(a.created_at))
+    // console.log(tweets);
+
+    tweets.reverse().forEach(tweet => {
       let $tweet = createTweetElement(tweet);
       $('#tweets').append($tweet);
       
     });
     
 
-  }
+  };
   const createTweetElement = function(tweet) {
-    let date = timeago.format(tweet.created_at)
-    // console.log('tweet', tweet) 
-    const $tweetMarkup = $(`<article>
+    let date = timeago.format(tweet.created_at);
+    // console.log('tweet', tweet)
+    const $tweetMarkup = $(`<article class= "tweet">
     <header>
       <div>
         <img src=${tweet.user.avatars} alt ="avatar" >
@@ -82,32 +51,37 @@ $(document).ready(() => {
     return $tweetMarkup;
   };
 
-
+  loadTweets();
   //submit a tweet
-  const $form = $('#sub-form')
+
+  const $form = $('#sub-form');
   $form.on("submit", function(event) {
-    let $counter = $('.counter')
-    event.preventDefault()
-    let tweet = $('#tweet-text').val()
-    if($counter < 0) {
-      alert("Tweet is empty, please write your tweet!")
-      return
+   
+    event.preventDefault();
+    let $tweet = $('#tweet-text').val();
+    if ($tweet.length < 1) {
+      alert("Tweet is empty, please write your tweet!");
+      return;
     }
-    if(tweet.length > 140) {
-      alert("Your tweet is too long!")
-      return
+    if ($tweet.length > 140) {
+      alert("Your tweet is too long!");
+      return;
     }
-    $.ajax({ 
-      url: '/tweets', 
+    $.ajax({
+      url: '/tweets',
       method: "POST",
       data: $form.serialize(),
-      fail: function (error) {
+      success: function() {
+        $('#tweet-text').val("");
+        $('#tweets').empty();
+        loadTweets();
+      },
+      fail: function(error) {
         console.log("error: ", error);
       }
-    })
-   $('#tweet-text').val("")
-    
-  })
+    });
+ 
+  });
 });
 
 
