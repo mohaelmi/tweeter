8ab4765559063c5b715f2a1534ec7af6afd8a492
mohaelmi <moha.abdi1771@gmail.com>
2023-07-17 19:13:27 -0500

    responsive with tablet and pc/laptops

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -14,34 +14,34 @@ $(document).ready(() => {
     });
   };
 
-  const escape = function (tweet) {
-    let div = document.createElement("div")
-    div.appendChild( document.createTextNode(tweet) )
+  const escape = function(tweet) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(tweet));
     return div.innerHTML;
   };
 
 
   const renderTweets = function(tweets) {
-    //iterate over array of tweets reversely or from newest one
-    tweets.reverse().forEach(tweet => {
-      let $tweet = createTweetElement(tweet);
+    tweets.forEach((tweet, i) => {
+      //iterate and grab the most recent tweet and pass it to creatTweetElement function
+      let $tweet = createTweetElement(tweets[tweets.length - 1 - i]);
       $('#tweets').append($tweet);
+      $('.title-name').text(tweet.user.name);
       
     });
     
 
   };
   const createTweetElement = function(tweet) {
-    const $name = $('.title-name')
-    $name.text(tweet.user.name)
+    
     let date = timeago.format(tweet.created_at);
     const $tweetMarkup = $(`<article class= "tweet">
     <header>
       <div>
         <img src=${tweet.user.avatars} alt ="avatar" >
       </div>
-      <div class="name">${tweet.user.name}</div>
-      <div class="email">${tweet.user.handle}</div> 
+      <div class="name"> ${tweet.user.name}  </div>
+      <div class="email"> ${tweet.user.handle} </div> 
     </header>
     <div> ${escape(tweet.content.text)} </div>
     <footer>
@@ -66,16 +66,16 @@ $(document).ready(() => {
     event.preventDefault();
     let $tweet = $('#tweet-text').val();
     if ($tweet.length < 1) {
-      $('.display-error').empty()
-      $('.display-error').text("Tweet area is empty, please write your tweet.")
-      $('.error').slideDown()
+      $('.display-error').empty();
+      $('.display-error').text("Tweet area is empty, please write your tweet.");
+      $('.error').slideDown();
       
       return;
     }
     if ($tweet.length > 140) {
-      $('.display-error').empty()
-      $('.display-error').text("Your tweet is too long!")
-      $('.error').slideDown()
+      $('.display-error').empty();
+      $('.display-error').text("Your tweet is too long!");
+      $('.error').slideDown();
       return;
     }
     $.ajax({
@@ -86,7 +86,7 @@ $(document).ready(() => {
         
         $('#tweet-text').val("");
         $('#tweets').empty();
-        $('.error').slideUp()
+        $('.error').slideUp();
         loadTweets();
       },
       fail: function(error) {
@@ -97,8 +97,8 @@ $(document).ready(() => {
   });
 
   $('#new-tweet').on('click', ()=> {
-    $('#sub-form').toggle()
-  })
+    $('#sub-form').toggle();
+  });
 });
 
 
